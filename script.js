3ae155628a4d728af890a7acdf87c2d6cc362325
Michael Essam <maichelessam2001@gmail.com>
2025-08-03 17:48:40 +0300

    add files

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput =document.getElementById("task-input" );
+    const taskList =document.getElementById("task-list" );
+    function addTask (){
+       const taskText = taskInput.value.trim();
+       if (taskText === ""){
+        alert("Please Enter The Task");
+       }else{
+        let li = document.createElement("li");
+        li.textContent = taskText;
+        let remove = document.createElement("button");
+        remove.textContent="Remove";
+        remove.className='remove-btn';
+        remove.onclick = function(){
+            remove.parentElement.remove()
+        }
+        li.appendChild(remove)
+        taskList.appendChild(li)
+        taskInput.value = ""
+       }    
+    }
+    addButton.addEventListener("click", addTask)
+})
